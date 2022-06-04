748426c55022f5cfe7f2572a2cac95ad85c5f7bc
Alexander Gagua <105374893+alexandergagua@users.noreply.github.com>
2022-06-04 14:08:45 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <style type="text/css">
       .header {
         position: absolute;
-        
         left: 50%;
         top: 44%;
         transform: translate(-50%, -44%);
@@ -18,16 +17,47 @@
         top: 56%;
         transform: translate(-50%, -44%);
         -webkit-transform: translate(-50%, -44%)
+      } 
+      .game{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
       }
     </style>
   </head>
 
   <body>
+  <div class="game">
+<?php
+$rand = rand(1,10);
+$guess = $_POST['guess'];
+$submit = $_POST['submit'];
+
+if(isset($submit)){
+if($guess == NULL){
+  header("location: index.php");
+  exit();
+}else{
+if($guess !=$rand){
+  echo " You lose, the correct answer is " . $rand. "<br/><a href='index.php'> Try Again </a>";
+} else {
+  echo " You Won :) <br/><a href='index.php'> Try Again ? ;)</a>";
+  }
+}
+
+} else {
+  header("location: index.php");
+  exit();
+}
+?>
+</div>
     <div class="header">
     <h1> Guessing Game ! <br/>- Play With Me -</h1>
     </div>
     <div class="guess">
-      <form action="game.php" method="POST">
+      <form action="index.php" method="POST">
         <input
           type="number"
           name="guess"
