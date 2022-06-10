76827e39356ed23e55af474209aecd2fbc2b7b68
Alexander Gagua <105374893+alexandergagua@users.noreply.github.com>
2022-06-10 20:11:01 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,74 +1,40 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Guessing Game</title>
-    <style type="text/css">
-      .header {
-        position: absolute;
-        left: 50%;
-        top: 44%;
-        transform: translate(-50%, -44%);
-        -webkit-transform: translate(-50%, -44%);
-      }
-
-      .guess {
-        position: absolute;
-        left: 50%;
-        top: 56%;
-        transform: translate(-50%, -44%);
-        -webkit-transform: translate(-50%, -44%)
-      } 
-      .game{
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        -webkit-transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-
-  <body>
-  <div class="game">
 <?php
-$rand = rand(1,10);
-$guess = $_POST['guess'];
-$submit = $_POST['submit'];
 
-if(isset($submit)){
-if($guess == NULL){
-  header("location: index.php");
-  exit();
-}else{
-if($guess !=$rand){
-  echo " You lose, the correct answer is " . $rand. "<br/><a href='index.php'> Try Again </a>";
-} else {
-  echo " You Won :) <br/><a href='index.php'> Try Again ? ;)</a>";
-  }
-}
+$number= $_POST['number_entered'];
 
-} else {
-  header("location: index.php");
-  exit();
-}
+$submitbutton= $_POST['submit'];
+
+
+$randomnumber= mt_rand(1,10);
+
+
 ?>
-</div>
-    <div class="header">
-    <h1> Guessing Game ! <br/>- Play With Me -</h1>
-    </div>
-    <div class="guess">
-      <form action="index.php" method="POST">
-        <input
-          type="number"
-          name="guess"
-          min="1"
-          max="10"
-          style="width: 220px";
-          placeholder="Guess The Number Lasha (1-10)"
-        /><br /><br />
 
-        <input type="submit" name="submit" style="position: absolute; left: 35%;" value="Guess"/>
-      </form>
-    </div>
-  </body>
-</html>
+
+<form action="" method="POST">
+Guess a Number Between 1 and 10: 
+<input type="text" name="number_entered" value='6'/> <br><br>
+
+Result: 
+<?php 
+if ($submitbutton){
+
+if (($number > 0) && ($number <11)){
+if ($number != $randomnumber)
+{
+echo "Incorrect guess. The correct number was $randomnumber. Try again";
+}
+else 
+{
+echo "$randomnumber is the correct guess. You got it right.";
+}
+}
+
+}
+
+?>
+<br><br>
+<input type="submit" name="submit" value="Search"/><br><br>
+</form>
+
+
